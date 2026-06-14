3bf69c925df1757b066d1b9c62a7385001935e37
Andrey_Kitaev <rozmadza@mail.ru>
2026-06-14 17:55:22 +0300

    change project

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -16,8 +16,8 @@ class BirthdayForm(forms.ModelForm):
     class Meta:
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
+        exclude = ('author',)
         # Указываем, что надо отобразить все поля.
-        fields = '__all__'
         widgets = {
             'birthday': forms.DateInput(
                 attrs={'type': 'date'},
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,5 @@
 # birthday/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
@@ -8,24 +9,39 @@ from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
-class BirthdayListView(ListView):
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
+class BirthdayListView(LoginRequiredMixin, ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
-class BirthdayUpdateView(UpdateView):
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
+
+class BirthdayUpdateView(LoginRequiredMixin, OnlyAuthorMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayDetailView(DetailView):
+class BirthdayDetailView(LoginRequiredMixin, DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
